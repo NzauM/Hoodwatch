d3593e268090b48bde23d49585fd88f54cd7b284
NzauM <mercywaenu16@gmail.com>
2020-01-19 20:38:58 +0300

    Add a test case for the profile class

--- hood/tests.py
+++ hood/tests.py
@@ -45,5 +45,43 @@ class Neighborhoodtestcase(TestCase):
         hoodlist = Neighborhood.objects.all()
         self.assertEqual(len(hoodlist,0))
 
+
+class Profiletestcase(TestCase):
+    '''
+    Class to test the profile class methods
+    '''
+    def setUp(self):
+        self.resident=User(username='resident',email='resident@hood.com')
+        self.profile = Profile(user=self.resident,profile_pic='img.jpg',hood='Karen')
+
+    def test_instance(self):
+        '''
+        Test class to check instantiation of a new profile instance
+        '''
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def tearDown(self):
+        '''
+        Class to delete all test instances after tests finish running
+        '''
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+
+    def create_user_test(self):
+        self.profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)==1)
+
+    def delete_profile_test(self):
+        '''
+        Tests that a profile instance can be deleted succesfully
+        '''
+        self.profile.save()
+        self.hood.delete()
+        profiles = Profile.objects.all()
+        self.assertEqual(len(profiles,0))
+
+    
+
     
 
